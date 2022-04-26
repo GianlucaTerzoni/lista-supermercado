3626e5b386aafbf7e6ccbe7c5686dfa3a507e490
GianlucaTerzoni <gianlucaterzoni@hotmail.com>
2022-04-26 11:00:05 -0300

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai'
+
+//mostrar un formulario para agregar items a la lista
+//se debe poder eliminar cada item de la lista
 
 function App() {
+    const[items, setItems] = useState([])
+    const[input, setInput] = useState('')
+
+    //agregar Items
+
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      const addItem = {
+        id: Math.floor(Math.random() * 1000),
+        text: input,
+        completed: false
+      }
+
+      setItems([...items, addItem])
+      setInput('')
+    }
+
+    //eliminar items 
+
+    const deleteItem = (id) => {
+      let filteredItems=[...items].filter((items) => items.id !== id)
+      setItems(filteredItems)
+      console.log('item deleted')
+    }
+
+    //tachar item
+
+    const toggleComplete = (id) => {
+      setItems(
+        items.map(item => (
+          item.id === id ? {...item, completed: !item.completed} : item
+        ))
+      )
+    }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">  
+        <div className="title">
+          <h1>Supermarket List</h1>
+          <p className="length">{(items < 1) ? 'Item(s)' : `Items: ${items.length}`}</p> 
+        </div>
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+              <div className="form-input">
+                <input 
+                value={input}
+                onChange={event => setInput(event.target.value)}
+                placeholder=''
+                type='text'
+                
+                />
+                <div className="button">
+                <button type="submit" className="btn btn-primary">Add Item</button>
+              </div> 
+              </div>
+                
+              
+            </form>
+          <div>
+            {items.map(item =>(
+              <div className={`item-row ${item.completed ? 'completed' : '' }`} key={item.id} onDoubleClick={() => toggleComplete(item.id)}>
+                <p>{item.text}</p>
+              <AiOutlineClose onClick={() => deleteItem(item.id)} className="icon" />
+              </div>
+            ))}
+          </div>
+
+               
+        </div>
+      </div>
+ 
+       
   );
 }
+
+
+
 
 export default App;
